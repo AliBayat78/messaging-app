dbb8e913aeecf33fc5433ef801e15afd40dbfa6e
Ali Bayat <ali.bayat77@yahoo.com>
2023-02-25 19:25:16 +0330

    tailwind added, Create New Id added, uuid added

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,34 +1,41 @@
 import * as React from 'react'
-import { Formik, FormikHelpers, FormikProps, Form, Field, FieldProps } from 'formik'
-
-interface MyFormValues {
-  userId: string
-}
+import { v4 as uuidV4 } from 'uuid'
 
 type LoginType = {
   onSubmitId: React.Dispatch<React.SetStateAction<string>>
 }
 
 const Login = ({ onSubmitId }: LoginType) => {
-  const initialValues: MyFormValues = { userId: '' }
+  const createNewId = () => {
+    onSubmitId(uuidV4())
+  }
 
   return (
-    <div>
-      <h1>My Example</h1>
-      <Formik
-        initialValues={initialValues}
-        onSubmit={(values, actions) => {
-          console.log({ values, actions })
-          onSubmitId(values.userId)
-          actions.setSubmitting(false)
-        }}
-      >
-        <Form>
-          <label htmlFor="userId">First Name</label>
-          <Field id="userId" name="userId" placeholder="First Name" />
-          <button type="submit">Submit</button>
-        </Form>
-      </Formik>
+    <div className="ml-4 w-full">
+      <h1>Enter Your Id</h1>
+      <div className="flex flex-col w-2/3">
+        <input
+          onChange={(e) => onSubmitId(e.target.value)}
+          className="w-1/3 p-4 rounded-lg"
+          id="userId"
+          name="userId"
+          placeholder="Enter Your Id"
+        />
+        <div className="flex flex-row justify-around mt-2 w-1/3">
+          <button
+            className="p-2 rounded-md bg-blue-700 text-white border-none cursor-pointer"
+            type="submit"
+          >
+            Login
+          </button>
+          <button
+            onClick={() => createNewId()}
+            className="p-2 rounded-md bg-gray-700 text-white border-none cursor-pointer"
+          >
+            Create New Id
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
